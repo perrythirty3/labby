c1662b7d466fa800c4a486330e16b3483ed74d62
PerryCode3 <pnguyen9395@gmail.com>
2025-09-09 16:49:38 -0400

    tes

---
+++ ECR/ecr.tf
@@ -0,0 +1,11 @@
+resource "aws_ecr_repository" "app" {
+name = var.ecr_repo_name
+image_tag_mutability = "MUTABLE"
+image_scanning_configuration { scan_on_push = true }
+encryption_configuration { encryption_type = "AES256" }
+}
+
+
+output "ecr_repo_url" {
+value = aws_ecr_repository.app.repository_url
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" { type = string }
+variable "ecr_repo_name" { type = string }
+variable "ecs_cluster_name" { type = string }
+variable "ecs_service_name" { type = string }
+variable "app_container_port" { type = number default = 80 }
+variable "desired_count" { type = number default = 1 }
+variable "vpc_id" { type = string }
+variable "public_subnet_ids" { type = list(string) }
