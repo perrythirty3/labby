230677c952eeeba320d19b042185461c73beac06
Perry <PerryCode3@users.noreply.github.com>
2025-09-11 13:24:36 -0400

    ecr.tf and main.tf changes

--- ecr.tf
+++ ecr.tf
@@ -1,8 +1,12 @@
 # ecr.tf
 resource "aws_ecr_repository" "app" {
   name = var.ecr_repo_name
-}
 
-output "ecr_repo_url" {
-  value = aws_ecr_repository.app.repository_url
+  # ✅ scan on push
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  # ✅ immutable tags so “latest” can’t be overwritten
+  image_tag_mutability = "IMMUTABLE"
 }
--- sg.tf
+++ sg.tf
@@ -1,3 +1,8 @@
+# main.tf or network.tf
+data "aws_vpc" "selected" {
+  id = var.vpc_id
+}
+
 # sg.tf
 resource "aws_security_group" "app" {
   name        = "${var.ecs_service_name}-sg"
@@ -8,13 +13,14 @@ resource "aws_security_group" "app" {
     from_port   = var.app_container_port
     to_port     = var.app_container_port
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip] # CI sets TF_VAR_my_ip
+    cidr_blocks = [var.my_ip] # CI injects TF_VAR_my_ip
   }
 
+  # ✅ Restrict egress to your VPC instead of 0.0.0.0/0
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [data.aws_vpc.selected.cidr_block]
   }
 }
