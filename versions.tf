fefbca8f919ad986eae6327b956324470527aaa5
Perry <PerryCode3@users.noreply.github.com>
2025-09-16 12:48:58 -0400

    TFLint tidy: remove unused data blocks, wire image_tag, use http_cidr, add random provider constraint

--- versions.tf
+++ versions.tf
@@ -1,13 +1,6 @@
 terraform {
   required_version = ">= 1.6.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
   backend "s3" {
     bucket  = "p-terraform-state-prod-681833711197"
     key     = "terraform.tfstate" # workspaces will prefix this
